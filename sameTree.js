c570ca1a04de58664df5c54697a525bec089947b
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Pro.local>
2021-02-12 19:46:29 -0800

    Added sameTree task

---
+++ sameTree.js
@@ -0,0 +1,8 @@
+var isSameTree = function(p, q) {
+  if(!p && !q){
+    return true;
+  } else if(!p || !q || p.val !== q.val){
+    return false;
+  } 
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};

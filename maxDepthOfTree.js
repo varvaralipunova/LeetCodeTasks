95fdf49e290a3e88680c9b8bc45817af4e672766
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Pro.local>
2021-02-14 21:40:08 -0800

    Updated maxDepthOfTree code

--- maxDepthOfTree.js
+++ maxDepthOfTree.js
@@ -11,20 +11,6 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-  if(!root){
-      return 0;
-  } 
-  let maxDepth = 1;
-  
-  const depthFirstTraverse=(node,depth=1)=>{
-      if(!node){
-          return
-      }
-      maxDepth = Math.max(maxDepth, depth);
-      
-      depthFirstTraverse(node.left, depth+1);
-      depthFirstTraverse(node.right, depth+1);
-  }
-  depthFirstTraverse(root);
-  return maxDepth;
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
